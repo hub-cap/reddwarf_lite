11240391fb2e67cc1fef0261a4c6967a133a73b5
Nirmal Ranganathan <rnirmal@gmail.com>
2012-08-14 12:52:50 -0500

    Removed the unused test files and moved the build_templates outside test Changed the following my.cnf defaults - open_files_limit = {3276} - max_allowed_packet = 16M - Enabled ha_innodb_driver by default

--- contrib/dbaas-mycnf/build_templates.sh
+++ contrib/dbaas-mycnf/build_templates.sh
@@ -1,13 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+# Build the debian package for my.cnf templates
+
+# chdir to the script dir
+self="${0#./}"
+base="${self%/*}"
+current=`pwd`
+
+if [ "$base" = "$self" ] ; then
+    home=$current
+elif [[ $base =~ ^/ ]]; then
+    home="$base"
+else
+    home="$current/$base"
+fi
+
+cd $home
 
 # Define the various templates
-MEMSIZE=( "512M:1" "1G:2" "2G:4" "4G:8" "8G:16" "16G:32" "32G:64" )
+MEMSIZE=( "512M:1" "1024M:2" "2048M:4" "4096M:8" "8192M:16" "16384M:32" "32768M:64" )
+
 
 # Create the individual templates from the master template
 for i in "${MEMSIZE[@]}"; do
     key=${i%%:*}
     multiplier=${i##*:}
-    cat ../etc/my.cnf.base | while read line; do
+
+    # Clear out the existing template
+    rm $home/etc/my.cnf.$key
+
+    cat $home/etc/my.cnf.base | while read line; do
         if [[ `expr "$line" : ".*{.*}"` != "0" ]]; then
             oldval=`echo $line | sed -e 's/.*{\(.*\)}.*/\1/'`
             prop=`echo $line | sed -e 's/^\(.*\) = {100}/\1/'`
@@ -18,7 +39,7 @@ for i in "${MEMSIZE[@]}"; do
             fi
             line=`echo $line | sed -e "s/{$oldval}/$newval/"`
         fi
-        echo $line >> etc/my.cnf.$key
+        echo $line >> $home/etc/my.cnf.$key
     done
 done
 
